c8716f277000d8797bfc56a52fe6728557ed0757
farhanhyper <farhanhyper@gmail.com>
2017-01-09 23:51:05 +0330

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -239,7 +239,7 @@ function create_config( )
     "time",
     "welcome"
     },
-    sudo_users = {144658773,114934518,94746365},--Sudo users
+    sudo_users = {91689297},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V2 Beta
@@ -343,10 +343,6 @@ _________________________________________
 _________________________________________
 _________________________________________
 _________________________________________
-ليست سودوها :@Cia_00_07
-@sina7sk
-@amirho3in
-]]
   }
   serialize_to_file(config, './data/config.lua')
   print('saved config into ./data/config.lua')
